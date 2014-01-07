56abb88ecc3b50bd704d202266c02689ed6af207
Nathan Pinsker <npinsker@mit.edu>
2014-01-07 18:49:12 -0500

    wtf

--- teams/examplejurgzplayer/RobotPlayer.java
+++ teams/examplejurgzplayer/RobotPlayer.java
@@ -1,45 +1,5 @@
 package examplejurgzplayer;
 
-import battlecode.common.Clock;
-import battlecode.common.GameActionException;
-import battlecode.common.RobotController;
-import examplejurgzplayer.hq.HQBehavior;
-import examplejurgzplayer.soldiers.SoldierBehavior;
-import examplejurgzplayer.utils.Utils;
-
-public class RobotPlayer {
-  public static void run(RobotController rc) {
-    Utils.initUtils(rc);
-    RobotBehavior robot = null;
-    switch (rc.getType()) {
-      case HQ:
-        // Strategy strategy = Strategy.decide();
-        robot = new HQBehavior();
-        break;
-      case SOLDIER:
-        robot = new SoldierBehavior();
-        break;
-      default: // change
-        robot = new SoldierBehavior();
-        break;
-    }
-
-    while (true) {
-      try {
-        robot.beginRound();
-        robot.run();
-        robot.endRound();
-        rc.yield();
-      } catch (GameActionException e) {
-        System.out.println("Round number = " + Clock.getRoundNum());
-        e.printStackTrace();
-      }
-    }
-  }
-}
-=======
-package examplejurgzplayer;
-
 import battlecode.common.Direction;
 import battlecode.common.GameConstants;
 import battlecode.common.RobotController;
