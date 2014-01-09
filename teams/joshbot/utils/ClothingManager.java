25fbee5999d50051f3a1a4c69e0ce9f860403d2b
jalman <jalman@mit.edu>
2014-01-09 02:33:46 -0500

    joshbot2.00
    boom!

---
+++ teams/joshbot/utils/ClothingManager.java
@@ -0,0 +1,18 @@
+package joshbot.utils;
+
+import battlecode.common.GameActionException;
+
+public class ClothingManager extends Utils {
+
+	public ClothingManager() {
+		super();
+	}
+	void getDressed() {
+		try {
+			RC.wearHat();
+		} catch (GameActionException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
