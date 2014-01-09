138d88b2a218afca2756569811907ffae700b783
jiangty <jiangtyd@gmail.com>
2014-01-09 07:08:39 -0500

    fixed examplejurgzplayer 0.1

---
+++ teams/examplejurgzplayer/NoiseTowerBehavior.java
@@ -0,0 +1,32 @@
+package examplejurgzplayer;
+
+import battlecode.common.GameActionException;
+
+public class NoiseTowerBehavior extends RobotBehavior {
+	public NoiseTowerBehavior() {
+	}
+
+	/**
+	 * Called at the beginning of each round.
+	 */
+  @Override
+  public void beginRound() throws GameActionException {
+
+  }
+
+	/**
+	 * Called every round.
+	 */
+  @Override
+  public void run() throws GameActionException {
+
+  }
+
+	/**
+	 * Called at the end of each round.
+	 */
+	@Override
+  public void endRound() {
+
+  }
+}
