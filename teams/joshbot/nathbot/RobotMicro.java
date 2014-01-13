6389797f38cca9c6db521875f973988c69c13f2a
jalman <jalman@mit.edu>
2014-01-13 14:55:12 -0500

    made micro for joshbot okay

--- teams/joshbot/nathbot/RobotMicro.java
+++ teams/joshbot/nathbot/RobotMicro.java
@@ -1,38 +1,55 @@
 package joshbot.nathbot;
 
-import java.util.Random;
-
 import battlecode.common.*;
+
+import java.util.*;
 
 public class RobotMicro {
 	static Random rand;
-
+	
 	public static boolean luge(RobotController rc) {
 		Team player = rc.getTeam();
 		Team enemy = rc.getTeam().equals(Team.A) ? Team.B : Team.A;
-		MapLocation origin = new MapLocation(0, 0);
-
+		
 		MapLocation myHQ = rc.senseHQLocation();
 		MapLocation theirHQ = rc.senseEnemyHQLocation();
 
-		Robot[] nearbyTeam = rc.senseNearbyGameObjects(Robot.class, 17, player);
-		Robot[] nearbyEnemies = rc.senseNearbyGameObjects(Robot.class, 17, enemy);
-
+		Robot[] nearbyTeam = rc.senseNearbyGameObjects(Robot.class, 25, player);
+		Robot[] nearbyEnemies = rc.senseNearbyGameObjects(Robot.class, 25, enemy);
+		Robot[] enemiesInRange = rc.senseNearbyGameObjects(Robot.class, 10, enemy);
+		
+		int nearbyTeamCount = 0;
+		for (Robot r : nearbyTeam) {
+			try {
+				if(rc.senseRobotInfo(r).type == RobotType.SOLDIER) nearbyTeamCount++;
+			} catch (GameActionException e) {
+				e.printStackTrace();
+			}
+		}
+		
 		MapLocation loc = rc.getLocation();
-
-    if (nearbyTeam.length + 1 >= nearbyEnemies.length) {
-      if (nearbyEnemies.length == 0) {
-        // navigate towards where we want to go
-        return false;
-      }
-      else if (rc.isActive()) { // attack!
-				for (int i=0; i<nearbyEnemies.length; ++i) {
+		
+		rc.setIndicatorString(0, "team " + nearbyTeamCount + " enemy " + nearbyEnemies.length + "," + Clock.getRoundNum());
+		
+		if (nearbyTeamCount+1 >= nearbyEnemies.length) {
+			if ((nearbyEnemies.length == 0 || nearbyTeamCount-1 >= nearbyEnemies.length) && enemiesInRange.length == 0) {
+				//navigate towards where we want to go
+				rc.setIndicatorString(1, "moving forward");
+				return false;
+			}
+			else { //attack!
+				rc.setIndicatorString(1, "attacking");
+				String s = "";
+				for (int i=0; i<enemiesInRange.length; ++i) {
+					s += enemiesInRange[i].getID() + ",";
 					try {
-						RobotInfo tokill = rc.senseRobotInfo(nearbyEnemies[i]);
-						MapLocation killplace = tokill.location;
-						if(rc.canAttackSquare(killplace) && tokill.type != RobotType.HQ) {
-              rc.attackSquare(killplace);
-							break;
+						if (rc.isActive()) {
+							RobotInfo tokill = rc.senseRobotInfo(enemiesInRange[i]);
+							MapLocation killplace = tokill.location;
+							if(rc.canAttackSquare(killplace) && tokill.type != RobotType.HQ) {
+								rc.attackSquare(killplace);
+								break;
+							}
 						}
 					}
 					catch (GameActionException e) {
@@ -40,15 +57,17 @@ public class RobotMicro {
 						System.out.println("exception!1");
 					}
 				}
+				rc.setIndicatorString(2, s);
 				return true;
 			}
 		}
 		else {
+			rc.setIndicatorString(1, "retreating" + "," + Clock.getRoundNum());
 			int dx = 0, dy = 0;
 			for (int i=0; i<nearbyEnemies.length; ++i) {
 				try {
 					RobotInfo tokill = rc.senseRobotInfo(nearbyEnemies[i]);
-
+					
 					dx += tokill.location.x;
 					dy += tokill.location.y;
 				}
@@ -59,18 +78,21 @@ public class RobotMicro {
 			}
 			dx /= nearbyEnemies.length;
 			dy /= nearbyEnemies.length;
-
+			
 			Direction newDir = loc.directionTo(new MapLocation(2*loc.x - dx, 2*loc.y - dy));
-
+			
 			try {
+				rc.setIndicatorString(2, "" + rc.isActive() + "," + rc.canMove(newDir) + "," + newDir.dx + "," + newDir.dy + "," + Clock.getRoundNum());
 				if (rc.isActive() && newDir != Direction.NONE && newDir != Direction.OMNI) {
 					if (rc.canMove(newDir)) {
 						rc.move(newDir);
 					}
 					else if (rc.canMove(newDir.rotateLeft())) {
+						rc.setIndicatorString(2, "" + newDir.rotateLeft().dx + "," + newDir.rotateLeft().dy);
 						rc.move(newDir.rotateLeft());
 					}
 					else if (rc.canMove(newDir.rotateRight())) {
+						rc.setIndicatorString(2, "" + newDir.rotateRight().dx + "," + newDir.rotateRight().dy);
 						rc.move(newDir.rotateRight());
 					}
 				}
@@ -80,6 +102,7 @@ public class RobotMicro {
 				System.out.println("exception!3");
 			}
 		}
+		if (nearbyEnemies.length > 0) { }
 		return true;
 	}
 }
