ed9bc99c581ca5ffdba5aff7c84d24476c89ad4b
jalman <jalman@mit.edu>
2014-01-27 18:14:27 -0500

    BFS is as optimized as a clam

--- teams/mergebot/noise/BFSNoiseTower.java
+++ teams/mergebot/noise/BFSNoiseTower.java
@@ -7,7 +7,6 @@ import static mergebot.utils.Utils.*;
 
 abstract public class BFSNoiseTower extends RobotBehavior {
 	
-
 	static Direction[] directions = {Direction.NORTH, Direction.NORTH_EAST, Direction.EAST, Direction.SOUTH_EAST, Direction.SOUTH, Direction.SOUTH_WEST, Direction.WEST, Direction.NORTH_WEST};
 	public static final int[] yrangefornoise = { 17, 17, 17, 17, 16, 16, 16, 15, 15, 14, 14, 13, 12, 11, 10, 8, 6, 3 };
 	Direction[][] dir = new Direction[35][35];
@@ -23,10 +22,33 @@ abstract public class BFSNoiseTower extends RobotBehavior {
 		
 	  queue[0] = currentLocation;
 	  dir[17][17] = Direction.OMNI;
+
+
+      for(int i = 7; i >= 0; i--) {
+        Direction d = directions[i];
+        MapLocation loc = currentLocation.add(d);
+        TerrainTile there = RC.senseTerrainTile(loc);
+        if(!there.isTraversableAtHeight(RobotLevel.ON_GROUND)) continue;
+        int x = loc.x - currentLocation.x;
+        int y = loc.y - currentLocation.y;
+        dir[x+17][y+17] = d.opposite();
+        queue[at] = loc;
+        at++;
+      }
 	  
-	  for(int s = 0; s < at; s++) {
-	    for(int i = 7; i >= 0; i--) {
-	      MapLocation loc = queue[s].add(directions[i]);
+	  for(int s = 1; s < at; s++) {
+	    Direction initD = dir[queue[s].x - currentLocation.x + 17][queue[s].y - currentLocation.y + 17];
+	    Direction aD;
+	    Direction bD;
+	    if(initD.isDiagonal()) {
+	      aD = initD.rotateLeft().rotateLeft();
+	      bD = initD.rotateRight();
+	    } else {
+        aD = initD.rotateLeft().rotateLeft().rotateLeft();
+        bD = initD.rotateRight().rotateRight();
+      }
+	    for(Direction d = aD; d != bD; d = d.rotateLeft()) {
+	      MapLocation loc = queue[s].add(d);
 	      TerrainTile there = RC.senseTerrainTile(loc);
 	      if(!there.isTraversableAtHeight(RobotLevel.ON_GROUND)) continue;
 	      int x = loc.x - currentLocation.x;
@@ -34,7 +56,7 @@ abstract public class BFSNoiseTower extends RobotBehavior {
         if(x*x + y*y > 200) continue;
         
 	      if(dir[x+17][y+17] == null) {
-	        dir[x+17][y+17] = directions[i].opposite();
+	        dir[x+17][y+17] = d.opposite();
 	        queue[at] = loc;
 	        at++;
 	      }
