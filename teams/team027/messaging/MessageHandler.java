56ada7c2a77a8826816ebb7f6ac37dcd98bda0e2
jiangty <jiangtyd@gmail.com>
2014-01-26 23:07:08 -0500

    delete team027 for now

--- teams/team027/messaging/MessageHandler.java
+++
@@ -1,7 +0,0 @@
-package team027.messaging;
-
-import battlecode.common.GameActionException;
-
-public interface MessageHandler {
-  public void handleMessage(int[] message) throws GameActionException;
-}
