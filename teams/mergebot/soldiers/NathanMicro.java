db5ba9cc5296fb6af91b806f07ba4f27404a3521
Nathan R Pinsker <npinsker@mit.edu>
2014-01-28 23:00:47 -0500

    call for help earlier; go to battle only with more power differential

--- teams/mergebot/soldiers/NathanMicro.java
+++ teams/mergebot/soldiers/NathanMicro.java
@@ -16,6 +16,7 @@ public class NathanMicro {
   public static final int FIRE_RANGE_SQUARED = RobotType.SOLDIER.attackRadiusMaxSquared;
   public static final int HELP_DURATION = 8;
   public static final float ALLY_WEIGHT_DECAY = 12;
+  public static final float POWER_ADVANTAGE = 1.2f;
 
   /**
    * TODO give actual names and/or definitions for these constants so other people know what's going on
@@ -26,6 +27,9 @@ public class NathanMicro {
   public static boolean isHelpingOut = false;
   public static MapLocation helpingLoc = new MapLocation(0, 0);
   public static int lastHelpRequest = 0;
+  
+  public static NavAlg navAlg = new BugMoveFun2();
+  public static MapLocation dest = null;
 
   public static boolean luge(Mover mover) throws GameActionException {
     RobotInfo[] nearbyEnemies = getEnemyRobotInfo();
@@ -147,20 +151,32 @@ public class NathanMicro {
           }
           zyzzl += "///" + "nextWeight: " + nextWeight + " at (" + nextLoc.x + "," + nextLoc.y + "))";
 
-          if (!isHelpingOut && (nearbyEnemies.length == 0 || (enemiesInRange.length == 0 && allyWeight >= 1.10 * nextWeight))) {
+          if (!isHelpingOut && (nearbyEnemies.length == 0 || (enemiesInRange.length == 0 && allyWeight >= POWER_ADVANTAGE * nextWeight))) {
             // willing to move forward and attack!
             String ss = "";
             for (int z=0; z<nearbyEnemies.length; ++z) ss += "," + nearbyEnemies[z].location.x + "," + nearbyEnemies[z].location.y;
-            RC.setIndicatorString(2, "moving forward (enemy weight: " + nextWeight + " at (" + nextLoc.x + "," + nextLoc.y + "))" + (target == null ? " -- ceding control" : locToString(target)) + " | " + zyzzl + " | turn " + Clock.getRoundNum() );
+            String zzss = "moving forward (enemy weight: " + nextWeight + " at (" + nextLoc.x + "," + nextLoc.y + "))" + (target == null ? " -- ceding control" : locToString(target)) + " | " + zyzzl + " | turn " + Clock.getRoundNum();
+            RC.setIndicatorString(2, zzss);
             if (target != null) {
-              mover.setTarget(target);
-              mover.move();
-              return true;
+        		setTarget(target);
+        		Direction nextDir = navAlg.getNextDir();
+        		zzss += " going " + nextDir.dx + "," + nextDir.dy;
+                RC.setIndicatorString(2, zzss);
+
+        		for (int i=0; i<nearbyEnemies.length; ++i) {
+        			if (nearbyEnemies[i].location.distanceSquaredTo(currentLocation.add(nextDir)) <= FIRE_RANGE_SQUARED) {
+                        messagingSystem.writeMicroMessage(nearbyEnemies[i].location, 1);
+                		RC.setIndicatorString(2, zzss + "//" + locToString(nearbyEnemies[i].location));
+                        break;
+        			}
+        		}
+        		RC.move(nextDir);
+        		return true;
             } else {
               return false;
             }
           }
-          else if (isHelpingOut && allyWeight >= 1.10 * nextWeight && target == null) {
+          else if (isHelpingOut && allyWeight >= POWER_ADVANTAGE * nextWeight && target == null) {
             Direction newDir = currentLocation.directionTo(helpingLoc);
             if (RC.isActive() && newDir != Direction.NONE && newDir != Direction.OMNI) {
               // go straight towards the target point
@@ -183,15 +199,16 @@ public class NathanMicro {
               }
             }
           }
-          RC.setIndicatorString(2, "" + target + "," + nearestPastrLoc);
+          RC.setIndicatorString(2, "" + target + "," + nearestPastrLoc + "," + (target != null ? RC.canAttackSquare(target) : ""));
           if (target == null) {
             //attack a PASTR i guess
             if (nearestPastrLoc != null) {
               // PASTR_RANGE = 5
             	Direction newDir = currentLocation.directionTo(nearestPastrLoc);
             	if (RC.canMove(newDir) && currentLocation.add(newDir).distanceSquaredTo(ENEMY_HQ) < RobotType.HQ.attackRadiusMaxSquared) {
-            		mover.setTarget(nearestPastrLoc);
-            		mover.move();
+            		
+            		setTarget(nearestPastrLoc);
+            		RC.move(navAlg.getNextDir());
             	}
             	else {
             		MapLocation cowTarget =
@@ -225,7 +242,7 @@ public class NathanMicro {
             if (dd > 9) d = 4;
 
             int maxDmg = (int) (enemiesInRange.length * (d - 1) * RobotType.SOLDIER.attackPower);
-            RC.setIndicatorString(2, maxDmg + "," + RC.getHealth() + "," + allyWeight + "," + enemyWeight + " | " + GREAT_LUGE + "," + target.x + "," + target.y + " - round " + Clock.getRoundNum());
+            String sf = maxDmg + "," + RC.getHealth() + "," + allyWeight + "," + enemyWeight + " | " + (GREAT_LUGE ? "suicide" : "") + "," + target.x + "," + target.y + " - round " + Clock.getRoundNum();
             if (RC.getHealth() > maxDmg + 5 && allyWeight < enemyWeight - GREAT_LUGE_ASSIST) {
               // TEMPORARY CHANGE ME LATER
               //GREAT_LUGE = true;
@@ -245,6 +262,8 @@ public class NathanMicro {
               if (m.x != target.x || m.y != target.y) {
                 messagingSystem.writeMicroMessage(target, 1);
               }
+              sf += " | requesting aid " + target.x + "," + target.y;
+              RC.setIndicatorString(2, sf);
             }
           }
         }
@@ -284,6 +303,16 @@ public class NathanMicro {
 
 
     return true;
+  }
+  public static void setTarget(MapLocation loc) {
+	  if (!loc.equals(dest)) {
+		  dest = loc;
+		  navAlg.recompute(loc);
+	  }
+  }
+  public static boolean isEnemyHQScary() {
+	  return false;
+	  //return RC.senseObjectAtLocation(RC.)
   }
   public static int lazySqrt(int k) {
     if (k <= 1) return 1;
