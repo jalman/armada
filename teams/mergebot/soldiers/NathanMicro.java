b4b2127b91ee294e807a4717b70246a0bf2a402c
jiangty <jiangtyd@gmail.com>
2014-01-31 15:37:00 -0500

    reverted to BugMove in micro nav

--- teams/mergebot/soldiers/NathanMicro.java
+++ teams/mergebot/soldiers/NathanMicro.java
@@ -30,7 +30,8 @@ public class NathanMicro {
   public static MapLocation helpingLoc = new MapLocation(0, 0);
   public static int lastHelpRequest = 0;
 
-  public static NavAlg navAlg = new DumbMove(); // new BugMoveFun2();
+  public static NavAlg navAlg = new BugMoveFun2();
+  public static NavAlg dumbMove = new DumbMove();
   public static MapLocation dest = null;
 
   public static MapLocation m = new MapLocation(0, 0);
@@ -218,8 +219,8 @@ public class NathanMicro {
               }
             }
             else {
-              setTarget(currentLocation.add(newDir));
-              newDir = navAlg.getNextDir(); // variable reuse, sorry
+              dumbMove.recompute(currentLocation.add(newDir));
+              newDir = dumbMove.getNextDir(); // variable reuse, sorry
               if (newDir != Direction.NONE) {
                 RC.move(newDir);
               }
@@ -370,8 +371,8 @@ public class NathanMicro {
             return true;
           }
           else {
-            setTarget(currentLocation.add(newDir));
-            newDir = navAlg.getNextDir(); // variable reuse, sorry
+            dumbMove.recompute(currentLocation.add(newDir));
+            newDir = dumbMove.getNextDir(); // variable reuse, sorry
             if (newDir != Direction.NONE) {
               RC.move(newDir);
             }
@@ -433,10 +434,10 @@ public class NathanMicro {
     Robot[] around = RC.senseNearbyGameObjects(Robot.class, 2);
     double allyDamage = RC.getHealth() + LIFE_VALUE, enemyDamage = 0;
     double predictDamage = GameConstants.SELF_DESTRUCT_BASE_DAMAGE + GameConstants.SELF_DESTRUCT_DAMAGE_FACTOR * RC.getHealth();
-       
+
     for (int i=0; i<around.length; ++i) {
     RobotInfo ri = RC.senseRobotInfo(around[i]);
-         
+
       double damage = (predictDamage >= ri.health ? ri.health : predictDamage);
       if (ri.team == ALLY_TEAM) {
         allyDamage += (damage + (predictDamage >= ri.health ? LIFE_VALUE : 0));
@@ -469,7 +470,7 @@ public class NathanMicro {
 
           boolean inCombat = false;
           int bestDist = 100000;
-          
+
           for (int j = stuff.length; --j >= 0;) {
             int dd = soldierLoc.distanceSquaredTo(RC.senseRobotInfo(stuff[j]).location);
             bestDist = (dd < bestDist ? dd : bestDist);
